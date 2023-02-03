1189c035e3bc1c66fdb0c0077f1cf00d72a46c9c
Samuel Kim <samuelskim420@gmail.com>
2023-02-03 21:22:09 +0000

    finished largest_contiguous_sum part 1!

--- execution_murder.rb
+++ execution_murder.rb
@@ -1,41 +1,79 @@
-#Phase 1 my_min
+# #Phase 1 my_min
 
-def my_min(arr)
+# def my_min(arr)
     
     
-    arr.each_with_index do |e1, i1|
-        min = true
+#     arr.each_with_index do |e1, i1|
+#         min = true
 
-        arr.each_with_index do |e2, i2|
-           if e2 < e1
-            min = false
-           end
+#         arr.each_with_index do |e2, i2|
+#            if e2 < e1
+#             min = false
+#            end
 
 
-        end
+#         end
 
-        return e1 if min == true
-    end
+#         return e1 if min == true
+#     end
 
    
-end
+# end
 
 
-#Phase 2 my_min
+# #Phase 2 my_min
 
-def my_min(arr)
+# def my_min(arr)
 
-    arr.inject do |min, ele| 
-        if ele < min 
-            ele
+#     arr.inject do |min, ele| 
+#         if ele < min 
+#             ele
+#         else
+#             min
+#         end
+
+#     end
+
+# end 
+
+
+
+
+def largest_contiguous_subsum(arr)
+    subs = []
+    
+    (0...arr.length).each do |start| #5
+        (start..arr.length).each do |finish| #5
+            sub = arr[start..finish] # arr[5..5]
+            subs << sub
+        end
+    end
+
+    subs
+
+    subs_arr = subs.inject do |max, sub|
+        if sub.sum > max.sum
+            sub
         else
-            min
+            max
         end
 
     end
 
-end 
+    subs_arr.sum
+
+end
+
+list = [2,3,-6,7,-6,7]
+
+p largest_contiguous_subsum(list)
 
 
+# [2], [2,3] , [2,3,-6], [2,3,-6,7], [2,3,-6,7,-6], [2,3,-6,7,-6, 7] 
+# [3], [3,-6], [3,-6,-7],[3,-6,7,-6], [3,-6,7,-6,7] 
+# [-6], [-6,7], [-6,7,-6], [-6,7,-6,7]
+# [7], [7,-6], [7,-6,7]
+# [-6], [7]
+# [7]
 
 
