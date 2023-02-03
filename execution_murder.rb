963e30c3f5699eb24e22e245a6f3c675bb16d0ee
Samuel Kim <samuelskim420@gmail.com>
2023-02-03 20:03:42 +0000

    dumb phase 1

--- execution_murder.rb
+++ execution_murder.rb
@@ -1,3 +1,27 @@
+#Phase 1 my_min
+
+def my_min(arr)
+    
+    
+    arr.each_with_index do |e1, i1|
+        min = true
+
+        arr.each_with_index do |e2, i2|
+           if e2 < e1
+            min = false
+           end
+
+
+        end
+
+        return e1 if min == true
+    end
+
+   
+end
+
+
+#Phase 2 my_min
 
 def my_min(arr)
 
@@ -9,33 +33,9 @@ def my_min(arr)
         end
 
     end
+
 end 
 
 
-    arr.each_with_index do |ele1, i1|
-        arr.each_with_index do |ele2, i2| 
-
-            next if ele1 >= ele2
-            if el2 > el1
-                holder = ele1
-            else
-                holder = ele2
-            end
-    end
-
-end
 
 
-for i in arr.length do 
-    holder = 0
-    ele1 = arr[i]
-    ele2 = arr[i+1]
-    
-    if ele2 > ele1
-        ele1 = holder
-    else    
-        
-
-
-
-p  my_min([0,3,5,4,-5,10,1,90])
